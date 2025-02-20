0e64f166e7fcec2801b19facede20368c1c64533
Igordanilo03 <141951957+Igordanilo03@users.noreply.github.com>
2025-02-20 17:00:23 -0300

    atualização das validações

--- video/serializers.py
+++ video/serializers.py
@@ -1,47 +1,32 @@
 from rest_framework import serializers
 from video.models import Video, Category
-
+from .validators import validate_non_empty, validate_color, get_default_category
 
 class VideoModelSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Video
         fields = ['id', 'title', 'description', 'url', 'category']
-        
+
     def to_internal_value(self, data):
         ret = super().to_internal_value(data)
         if 'category' not in ret or ret['category'] is None:
-            try:
-                ret['category'] = Category.objects.get(id=1)
-            except Category.DoesNotExist:
-                raise serializers.ValidationError("Categoria padrão não encontrada.")
+            ret['category'] = get_default_category()
         return ret
-
-    def create(self, validated_data):
-        return Video.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         if 'category' not in validated_data or validated_data['category'] is None:
-            validated_data['category'] = Category.objects.get(id=1)
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+            validated_data['category'] = get_default_category()
+        instance.__dict__.update(validated_data)
         instance.save()
         return instance
-        
 
 class CategoryModelSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Category
         fields = ['id', 'title', 'color']
-        
+
     def validate_title(self, value):
-        if not value:
-            raise serializers.ValidationError("O campo é obrigatório")
-        return value
-    
+        return validate_non_empty(value, "título")
+
     def validate_color(self, value):
-        if not value:
-            raise serializers.ValidationError("O campo é obrigatório")
-        return value
+        return validate_color(value)
